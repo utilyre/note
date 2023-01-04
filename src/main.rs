75906533c9e6e8708bc0fd4827314a0218a6bb2f
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-04 16:10:03 +0330

    feat(args): use clap crate for parsing args

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
-use std::{env, fs::OpenOptions, io::Write, process};
+use std::{fs::OpenOptions, io::Write};
 
 use chrono::Local;
+use clap::Parser;
 use xdg::BaseDirectories;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Task to be noted
+    task: String,
+}
 
 fn main() {
     let xdg_dirs = BaseDirectories::with_prefix("note").unwrap();
 
     let notes_path = xdg_dirs.place_state_file("notes.csv").unwrap();
     // let notes_file = File::create(notes_path);
-
-    let args: Vec<_> = env::args().collect();
-    let task = args.get(1).unwrap_or_else(|| {
-        eprintln!("no task provided");
-        process::exit(2)
-    });
 
     let mut notes_file = OpenOptions::new()
         .create(true)
@@ -23,6 +25,7 @@ fn main() {
 
     // mm/dd/yy hh:mm:ss
     let datetime = Local::now().format("%Y-%m-%d %H:%M:%S");
+    let args = Args::parse();
 
-    writeln!(notes_file, "{},{}", datetime, task).unwrap();
+    writeln!(notes_file, "{},{}", datetime, args.task).unwrap();
 }
