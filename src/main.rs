666a8f24ea229bd1b27e5d5fd3b0e653329de846
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-04 16:28:24 +0330

    feat(args): implement `ls` subcommand

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,51 @@
-use std::{fs::OpenOptions, io::Write};
+use std::{
+    fs::{self, OpenOptions},
+    io::Write,
+};
 
 use chrono::Local;
 use clap::Parser;
 use xdg::BaseDirectories;
 
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    #[command(subcommand)]
+    action: Action,
+}
+
+#[derive(clap::Subcommand)]
+enum Action {
+    Add(AddArgs),
+    Ls,
+}
+
+#[derive(Parser)]
+struct AddArgs {
     /// Task to be noted
     task: String,
 }
 
 fn main() {
     let xdg_dirs = BaseDirectories::with_prefix("note").unwrap();
-
     let notes_path = xdg_dirs.place_state_file("notes.csv").unwrap();
-    // let notes_file = File::create(notes_path);
 
-    let mut notes_file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(notes_path)
-        .unwrap();
-
-    // mm/dd/yy hh:mm:ss
-    let datetime = Local::now().format("%Y-%m-%d %H:%M:%S");
     let args = Args::parse();
+    match args.action {
+        Action::Add(add_args) => {
+            let mut notes_file = OpenOptions::new()
+                .create(true)
+                .append(true)
+                .open(notes_path)
+                .unwrap();
 
-    writeln!(notes_file, "{},{}", datetime, args.task).unwrap();
+            let datetime = Local::now().format("%Y-%m-%d %H:%M:%S");
+            writeln!(notes_file, "{},{}", datetime, add_args.task).unwrap();
+        }
+
+        Action::Ls => {
+            let contents = fs::read_to_string(notes_path).unwrap();
+            println!("{}", contents);
+        }
+    }
 }
