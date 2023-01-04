629280c5d7ef1ba0d7474aad7d7eea6266bb7dd9
Amirabbas Fazelinia <utilyre@proton.me>
2023-01-04 16:02:01 +0330

    feat(proto): implement simple note insertion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{env, fs::OpenOptions, io::Write, process};
+
+use chrono::Local;
+use xdg::BaseDirectories;
+
 fn main() {
-    println!("Hello, world!");
+    let xdg_dirs = BaseDirectories::with_prefix("note").unwrap();
+
+    let notes_path = xdg_dirs.place_state_file("notes.csv").unwrap();
+    // let notes_file = File::create(notes_path);
+
+    let args: Vec<_> = env::args().collect();
+    let task = args.get(1).unwrap_or_else(|| {
+        eprintln!("no task provided");
+        process::exit(2)
+    });
+
+    let mut notes_file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(notes_path)
+        .unwrap();
+
+    // mm/dd/yy hh:mm:ss
+    let datetime = Local::now().format("%Y-%m-%d %H:%M:%S");
+
+    writeln!(notes_file, "{},{}", datetime, task).unwrap();
 }
